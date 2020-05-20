24e7ed473f7c41b6f32350756c2472aef8fd9122
sirui.csr <sirui.csr@alibaba-inc.com>
2020-05-21 03:32:09 +0800

    simplify

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,8 @@
 import React from "react";
 import "./App.css";
 
-import { Button, DatePicker } from "@alicloud/console-components";
-import AppLayout from "@alicloud/console-components-app-layout";
-import Page from "@alicloud/console-components-page";
-import ConsoleMenu from "@alicloud/console-components-console-menu";
-import "@alicloud/console-components/dist/wind.css";
-import moment from "moment";
-console.log(moment)
-
-const menuItems = [
-  {
-    key: "overview",
-    label: "概览",
-  },
-  {
-    key: "lists",
-    label: "列表",
-    items: [
-      {
-        key: "basic-list-1",
-        label: "基础列表1",
-      },
-      {
-        key: "basic-list-2",
-        label: "基础列表2",
-      },
-    ],
-  },
-  {
-    key: "basic-form",
-    label: "基础表单",
-  },
-];
-
 function App() {
-  return (
-    <div className="App">
-      <AppLayout nav={<ConsoleMenu items={menuItems} header="页面一级导航" />}>
-        <Page>
-          <Page.Header>页面内容</Page.Header>
-          <Page.Content>
-            <div>
-              <Button type="primary">使用Button</Button>
-            </div>
-            <div>
-              使用DatePicker：
-              <DatePicker></DatePicker>
-            </div>
-          </Page.Content>
-        </Page>
-      </AppLayout>
-    </div>
-  );
+  return <div className="App">test</div>;
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,6 @@ module.exports = {
   alias: {
     react: "@pika/react",
     "react-dom": "@pika/react-dom",
-    "styled-components":
-      "styled-components/dist/styled-components.browser.esm.js",
   },
   jsx: "react",
-  rollupInputOptions: {
-    // next目前用rollup的tree-shaking无法正确构建
-    // https://github.com/alibaba-fusion/next/issues/1862
-    treeshake: false,
-  },
-  rollupPluginCommonJSNamedExports: {
-    "react-sizeme": ["SizeMe"],
-  },
-  optimizeDeps: {
-    commonJSWhitelist: ["moment"],
-  },
 };
