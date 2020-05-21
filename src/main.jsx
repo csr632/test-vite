1c83b56310a659dbbd2ef33d433f3809c7723d8b
sirui.csr <sirui.csr@alibaba-inc.com>
2020-05-21 10:55:23 +0800

    simplify

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import v from "test-package";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+console.log(v)
