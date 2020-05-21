30fe4b386cb747e51405d2075b0479f98cb4a712
sirui.csr <sirui.csr@alibaba-inc.com>
2020-05-21 10:15:04 +0800

    reproduce issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import "./App.css";
 
 function App() {
-  return <div className="App">test</div>;
+  return <div className="App">
+    ：
+  </div>
 }
 
 export default App;
