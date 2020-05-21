4703db24566439664978db840e326d4c6872af79
sirui.csr <sirui.csr@alibaba-inc.com>
2020-05-21 16:35:25 +0800

    simplify

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,14 @@
 import React from "react";
 import "./App.css";
 
-import { Button, DatePicker } from "@alicloud/console-components";
-import AppLayout from "@alicloud/console-components-app-layout";
-import Page from "@alicloud/console-components-page";
-import ConsoleMenu from "@alicloud/console-components-console-menu";
-import "@alicloud/console-components/dist/wind.css";
-import moment from "moment";
-console.log(moment);
+import styled from "styled-components";
 
-const menuItems = [
-  {
-    key: "overview",
-    label: "概览",
-  },
-  {
-    key: "lists",
-    label: "列表",
-    items: [
-      {
-        key: "basic-list-1",
-        label: "基础列表1",
-      },
-      {
-        key: "basic-list-2",
-        label: "基础列表2",
-      },
-    ],
-  },
-  {
-    key: "basic-form",
-    label: "基础表单",
-  },
-];
-
-const { Breadcrumb } = Page;
-const breadcrumb = (
-  <Breadcrumb>
-    <Breadcrumb.Item>Home</Breadcrumb.Item>
-    <Breadcrumb.Item>Blog</Breadcrumb.Item>
-    <Breadcrumb.Item>Name It, and They Will Come</Breadcrumb.Item>
-  </Breadcrumb>
-);
+const SDiv = styled.div`
+  border: 1px solid red;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <AppLayout nav={<ConsoleMenu items={menuItems} header="页面一级导航" />}>
-        <Page>
-          <Page.Header title="页面内容" breadcrumb={breadcrumb}></Page.Header>
-          <Page.Content>
-            <div>
-              <Button type="primary">使用Button</Button>
-            </div>
-            <div style={{ marginTop: "24px" }}>
-              使用DatePicker
-              <DatePicker />
-            </div>
-          </Page.Content>
-        </Page>
-      </AppLayout>
-    </div>
-  );
+  return <SDiv className="App">hello</SDiv>;
 }
-
-console.log("：");
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,6 @@
-import replace from "@rollup/plugin-replace";
 import vpr from "vite-plugin-react";
 
-const replacedEnv = JSON.stringify(process.env.NODE_ENV || "development");
-
 module.exports = {
-  alias: {
-    "styled-components":
-      "styled-components/dist/styled-components.browser.esm.js",
-  },
   jsx: "react",
   plugins: [vpr],
-  rollupInputOptions: {
-    plugins: [
-      replace({
-        "window.process.env.NODE_ENV": replacedEnv,
-        "process.env.NODE_ENV": replacedEnv,
-        include: ["**/@alifd/next/es/util/env.js"],
-      }),
-    ],
-  },
-  rollupPluginCommonJSNamedExports: {
-    "react-sizeme": ["SizeMe"],
-  },
-  optimizeDeps: {
-    commonJSWhitelist: ["moment"],
-  },
 };
