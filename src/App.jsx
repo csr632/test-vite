2db39a89160ab1793d0931caef5f77b6b3118ee7
Sirui Chen <632882184@qq.com>
2020-05-23 15:47:27 +0800

    update deps

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import styled from "styled-components";
+
+const Box = styled.div`
+  border: 1px solid red;
+`;
+
 function App() {
-  return <div className="App">hello</div>;
+  return <Box className="App">hello</Box>;
 }
 
 export default App;
