fbb8f84f4e5ddbb6d6af8c900c5688f6393c2bfd
Sirui Chen <632882184@qq.com>
2020-05-24 16:50:27 +0800

    use vite with latest cjs plugin

--- vite.config.js
+++ vite.config.js
@@ -3,29 +3,19 @@ import replace from "@rollup/plugin-replace";
 const replacedEnv = JSON.stringify(process.env.NODE_ENV || "development");
 
 module.exports = {
-  alias: {
-    react: "@pika/react",
-    "react-dom": "@pika/react-dom",
-    "styled-components":
-      "styled-components/dist/styled-components.browser.esm.js",
-  },
   jsx: "react",
   rollupInputOptions: {
-    // next目前用rollup的tree-shaking无法正确构建
-    // https://github.com/alibaba-fusion/next/issues/1862
-    treeshake: false,
     plugins: [
       replace({
         "window.process.env.NODE_ENV": replacedEnv,
-        "process.env.NODE_ENV": replacedEnv,
         include: ["**/@alifd/next/es/util/env.js"],
+      }),
+      replace({
+        "process.env.NODE_ENV": replacedEnv,
       }),
     ],
   },
-  rollupPluginCommonJSNamedExports: {
-    "react-sizeme": ["SizeMe"],
-  },
   optimizeDeps: {
-    commonJSWhitelist: ["moment"],
+    commonJSWhitelist: ["react", "react-dom", "moment"],
   },
 };
