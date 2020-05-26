69d48f333c400e4e249d790af393cdc2105e46ea
sirui.csr <sirui.csr@alibaba-inc.com>
2020-05-26 11:34:45 +0800

    use vite-plugin-mdx

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,4 @@
-import * as mdx from "@mdx-js/mdx";
-import type { UserConfig } from "vite";
-import { ensureEsbuildService } from "vite";
-import { transformReactCode } from "vite-plugin-react/dist/transform";
-
-const DEFAULT_RENDERER = `
-import React from 'react'
-import { mdx } from '@mdx-js/react'
-`;
-
 module.exports = {
   jsx: "react",
-  plugins: [require("vite-plugin-react")],
-  transforms: [
-    {
-      test(path, query) {
-        if (/\.mdx?$/.test(path)) {
-          return true;
-        }
-        return false;
-      },
-      async transform(code, isImport, isBuild, path) {
-        const jsx = await mdx(code);
-        const esBuild = await ensureEsbuildService();
-        const { js } = await esBuild.transform(jsx, {
-          loader: "jsx",
-          target: "es2019",
-          jsxFactory: "mdx",
-        });
-        const result = transformReactCode(`${DEFAULT_RENDERER}\n${js}`, path);
-        return result;
-      },
-    },
-  ],
-} as UserConfig;
+  plugins: [require("vite-plugin-react"), require("vite-plugin-mdx")],
+};
