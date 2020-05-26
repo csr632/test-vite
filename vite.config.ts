1ba897468b5180f56d2f7a2e4ab4170a92007332
csr632 <632882184@qq.com>
2020-05-27 02:04:31 +0800

    update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,8 @@
+import type { UserConfig } from "vite";
+import mdx from "vite-plugin-mdx";
+import react from "vite-plugin-react";
+
 module.exports = {
   jsx: "react",
-  plugins: [require("vite-plugin-react"), require("vite-plugin-mdx")],
-};
+  plugins: [react, mdx],
+} as UserConfig;
