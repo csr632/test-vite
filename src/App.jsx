4532498b4fb6fb0aca3b699a2384a5045f476ff0
csr632 <632882184@qq.com>
2020-05-27 10:57:55 +0800

    reproduce https://github.com/vitejs/vite/issues/275

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import React from "react";
-import "./App.css";
-import Counter from "./Counter";
-
-import styled from "styled-components";
-
-const Box = styled.div`
-  border: 1px solid red;
-`;
 
 function App() {
-  return (
-    <Box className="App">
-      <p>Box2</p>
-      <Counter />
-    </Box>
-  );
+  return <p>Box2</p>;
 }
 
 export default App;
