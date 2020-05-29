9e90aade0ef90ee6e91c997ad5cebe4f5f1862e2
csr632 <632882184@qq.com>
2020-05-29 11:04:03 +0800

    update deps

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,4 @@ import * as vpr from "vite-plugin-react";
 module.exports = {
   jsx: "react",
   plugins: [vpr],
-  rollupInputOptions: {
-    plugins: [
-      {
-        name: "resolve-styled",
-        resolveId(id) {
-          if (id === "styled-components") {
-            return this.resolve(
-              "styled-components/dist/styled-components.browser.esm.js"
-            );
-          }
-        },
-      },
-    ],
-  },
 } as UserConfig;
