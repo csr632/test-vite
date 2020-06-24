c34ffc3905be1a35707b1b5a1d9dae617756b13e
csr632 <632882184@qq.com>
2020-06-24 14:44:06 +0800

    cjs package showcase: axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+// axios is a commonjs package
+import axios from "axios";
+
 import "./App.css";
 import Counter from "./Counter";
-
-import styled from "styled-components";
 
 const Box = styled.div`
   border: 1px solid red;
 `;
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://os.alipayobjects.com/rmsportal/ODDwqcDFTLAguOvWEolX.json")
+      .then((value) => {
+        setData(value.data[0].children[0]);
+      });
+  }, []);
+
   return (
     <Box className="App">
       <p>Box2</p>
       <Counter />
+      <hr />
+      <p>Load data using axios:</p>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Loading data...</p>
+      )}
     </Box>
   );
 }
