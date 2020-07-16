aa4f65f878b8d1bb0561ebd6852eba35ad4a0502
csr632 <632882184@qq.com>
2020-07-17 02:20:52 +0800

    reproduce issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Link to="/some-path.tsx">link</Link>
+      </div>
+      <Route path="/some-path.tsx">
+        <App />
+      </Route>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
