d7a2697ba57e9e8df4b65de3a195bc00f511fd3e
csr632 <632882184@qq.com>
2020-07-31 18:21:55 +0800

    reproduce issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,23 @@ import * as vpr from "vite-plugin-react";
 module.exports = {
   jsx: "react",
   plugins: [vpr],
+  rollupInputOptions: {
+    plugins: [
+      {
+        name: "test",
+        async resolveId(importee, importer) {
+          if (importee.endsWith("Counter")) {
+            const resolveRes = await this.resolve(importee, importer, {
+              skipSelf: true,
+            })!;
+            this.emitFile({
+              type: "chunk",
+              id: resolveRes?.id!,
+            });
+            return resolveRes;
+          }
+        },
+      },
+    ],
+  },
 } as UserConfig;
