83efdb9703d9777ae2ae763a05e7a84fcbe66255
Sirui Chen <632882184@qq.com>
2021-01-16 19:42:57 +0800

    improve alias config

--- packages/demos/vite.demos.ts
+++ packages/demos/vite.demos.ts
@@ -1,13 +1,11 @@
 import type { UserConfig } from 'vite'
-import * as path from 'path'
-
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 module.exports = {
   jsx: 'react',
   plugins: [reactRefresh()],
   alias: {
-    'my-button': path.resolve(__dirname, '../button/src'),
-    'my-card': path.resolve(__dirname, '../card/src')
+    'my-button': 'my-button/src/index.tsx',
+    'my-card': 'my-card/src/index.tsx'
   }
 } as UserConfig
